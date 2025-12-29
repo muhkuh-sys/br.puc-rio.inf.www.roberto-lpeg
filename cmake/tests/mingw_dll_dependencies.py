6b14e2763801dd78005c54e45c4380730d1c3f45
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2025-12-29 16:18:16 +0100

    Drop support for Lua5.1 and Windows, add Ubuntu 24.04 build.

--- cmake/tests/mingw_dll_dependencies.py
+++ cmake/tests/mingw_dll_dependencies.py
@@ -21,24 +21,36 @@ def main(argv):
     tParser = argparse.ArgumentParser(
         description='Check the DLL dependencies of an executable.'
     )
-    tParser.add_argument('infile', nargs='+',
-                         help='read the input data from INPUT_FILENAME',
-                         metavar='INPUT')
-    tParser.add_argument('-o', '--objdump',
-                         dest='strObjDump',
-                         default='objdump',
-                         help='use OBJDUMP to extract the dependencies',
-                         metavar='OBJDUMP')
-    tParser.add_argument('-v', '--verbose',
-                         dest='fVerbose',
-                         action='store_true',
-                         default=False,
-                         help='be more verbose')
-    tParser.add_argument('-u', '--userdll',
-                         dest='astrUserDlls',
-                         action='append',
-                         help='add DLL to the list of known user DLLs',
-                         metavar='DLL')
+    tParser.add_argument(
+        'infile',
+        nargs='+',
+        help='read the input data from INPUT_FILENAME',
+        metavar='INPUT'
+    )
+    tParser.add_argument(
+        '-o',
+        '--objdump',
+        dest='strObjDump',
+        default='objdump',
+        help='use OBJDUMP to extract the dependencies',
+        metavar='OBJDUMP'
+    )
+    tParser.add_argument(
+        '-v',
+        '--verbose',
+        dest='fVerbose',
+        action='store_true',
+        default=False,
+        help='be more verbose'
+    )
+    tParser.add_argument(
+        '-u',
+        '--userdll',
+        dest='astrUserDlls',
+        action='append',
+        help='add DLL to the list of known user DLLs',
+        metavar='DLL'
+    )
     aOptions = tParser.parse_args()
 
     # Build the combined list of DLLs.
@@ -54,12 +66,16 @@ def main(argv):
         # Get all DLL dependencies from the file.
         aCmd = [aOptions.strObjDump, '-p', strInFile]
         tProc = subprocess.Popen(aCmd, stdout=subprocess.PIPE)
-        strOutput = tProc.communicate()[0].decode("utf-8", "replace")
+        strOutput = tProc.communicate()[0].decode(
+            "utf-8",
+            "replace"
+        )
         if tProc.returncode != 0:
-            raise Exception('The command failed with return code %d: %s' % (
-                tProc.returncode,
-                ' '.join(aCmd)
-            ))
+            raise Exception(
+                'The command failed with return code %d: %s' % (
+                    tProc.returncode, ' '.join(aCmd)
+                )
+            )
 
         # Find all DLL dependencies.
         for tMatch in re.finditer(r'DLL Name:\s+(.+).dll', strOutput):
@@ -69,8 +85,10 @@ def main(argv):
             if aOptions.fVerbose is True:
                 print('Found dependency %s.dll' % strDll)
             if strDll not in astrAllowedDlls:
-                raise Exception('The DLL dependency %s is not part of the '
-                                'standard system DLLs.' % strDll)
+                raise Exception(
+                    'The DLL %s is not part of the standard system DLLs.' %
+                    strDll
+                )
 
 
 if __name__ == '__main__':
